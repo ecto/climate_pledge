b81b2a54a595cdf1083eb5b4708515f4fccab123
Cam Pedersen <cam@campedersen.com>
2024-04-09 07:32:05 -0400

    readme formatting

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-![The CLimate Pledge](https://www.theclimatepledge.com/content/dam/amazonclimatepledge/logos/new-logo-color.svg)
+![The Climate Pledge](https://www.theclimatepledge.com/content/dam/amazonclimatepledge/logos/new-logo-color.svg)
 
-The Climate Pledge is a commitment to be net-zero carbon by 2040—a decade ahead of the Paris Agreement’s goal of 2050.
+The Climate Pledge is a commitment to be net-zero carbon by 2040 — a decade ahead of the Paris Agreement’s goal of 2050.
 
 This Ruby Gem fetches the latest climate pledge signatories from the [Climate Pledge website](https://www.theclimatepledge.com/).
+
+This Gem is not affiliated with The Climate Pledge or Amazon.
 
 ## Installation
 
@@ -55,6 +57,7 @@ ClimatePledge.signatories
 #   :industry=>"Materials",
 #   :join_date=>#<Date: 2020-11-15 ((2459169j,0s,0n),+0s,2299161j)>},
 # ...
+```
 
 ## Contributing
 
--- lib/climate_pledge.rb
+++ lib/climate_pledge.rb
@@ -2,15 +2,28 @@ require "open-uri"
 require "json"
 require "date"
 
+# A client for Climate Pledge data.
+# More information about the Climate Pledge can be found at:
+# https://www.theclimatepledge.com/
 class ClimatePledge
   BASE = "https://www.theclimatepledge.com"
   SIGNATORY_LIST = "#{BASE}/content/amazonclimatepledge/us/en/Signatories/jcr:content/root/responsivegrid/c19_grid.signatoryList.json"
 
+  # Returns a list of signatories to the Climate Pledge,
+  # first formatting the data for each signatory.
   def self.signatories
     json = JSON.load(URI.open(SIGNATORY_LIST))
     json["signatories"]&.map(&method(:format_signatory))
   end
 
+  # Formats a signatory for display.
+  # The expected signatory input format is a hash with the following keys:
+  # - name
+  # - logoSrc
+  # - link
+  # - country
+  # - industryTag
+  # - joinDate
   def self.format_signatory(signatory)
     {
       name: signatory["name"],
